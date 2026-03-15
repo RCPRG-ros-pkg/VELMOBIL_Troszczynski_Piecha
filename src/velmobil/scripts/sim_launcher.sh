f6b63d002e0cd81eb6c345965ec91ac597f8f3d7
Miłosz Piecha <01186986@pw.edu.pl>
2026-03-15 23:57:19 +0100

    packages cleanup

--- src/velmobil/scripts/sim_launcher.sh
+++
@@ -1,4 +0,0 @@
-cd ../../..
-colcon build --packages-select velmobil
-source install/setup.bash
-ros2 launch velmobil simple_velmobil.launch.py
--- src/velmobil_simulation/launch/simple_velmobil.launch.py
+++ src/velmobil_simulation/launch/simple_velmobil.launch.py
@@ -3,53 +3,69 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    world_file_arg = DeclareLaunchArgument(
-        'world',
-        default_value=PathJoinSubstitution([
-            FindPackageShare('velmobil'), 
-            'worlds', 
-            'basic.sdf'
-        ]),
-        description='Full path to the SDF world file to load'
-    )
-    velmobil_pkg_path = get_package_share_directory("velmobil")
-    rviz_config_path = os.path.join(velmobil_pkg_path, 'config', 'config.rviz')
-    bridge_config = os.path.join(
-        get_package_share_directory('velmobil'),
-        'config',
+    # PACKAGES
+    velmobil_description = FindPackageShare('velmobil_description')
+    velmobil_simulation = FindPackageShare('velmobil_simulation')
+    ros_gz_sim = FindPackageShare('ros_gz_sim')
+
+
+    # CONFIG FILES
+    rviz_config_path = PathJoinSubstitution([
+        velmobil_simulation,
+        'rviz',
+        'basic.rviz'
+    ])
+    bridge_config = PathJoinSubstitution([
+        velmobil_simulation,
+        'gz_bridge',
         'bridge_config.yaml'
-    )
+    ])
+    robot_controllers = PathJoinSubstitution([
+        velmobil_description,
+        'control',
+        'simple_velmobil.yaml'
+    ])
+    simple_velmobil_urdf = PathJoinSubstitution([
+        velmobil_description,
+        'urdf',
+        'simple_velmobil.urdf.xacro'
+    ])
+
+
+
+    # ARGUMENTS
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    rviz = LaunchConfiguration('rviz', default=True)
     world = LaunchConfiguration('world')
 
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
-            PathJoinSubstitution(
-                [FindPackageShare('velmobil'),
-                 'urdf', 'simple_velmobil.xacro.urdf']
-            ),
-        ]
-    )
-    robot_description = {'robot_description': robot_description_content}
-    
-    robot_controllers = PathJoinSubstitution(
-        [FindPackageShare('velmobil'), 'config', 'simple_velmobil.yaml']
-    )
 
-    node_robot_state_publisher = Node(
+    # XACRO COMMAND
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name='xacro')]),
+        ' ',
+        simple_velmobil_urdf,
+    ])
+    
+
+
+    # NODES
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[robot_description, {'use_sim_time': use_sim_time}]
+        parameters=[
+            {'robot_description': robot_description_content},
+            {'use_sim_time': use_sim_time}
+        ]
     )
 
     gz_spawn_entity = Node(
@@ -87,16 +103,42 @@ def generate_launch_description():
         arguments=['-d', rviz_config_path],
         parameters=[
             {'use_sim_time': use_sim_time},
-            robot_description
-        ]
+        ],
+        condition=IfCondition(rviz) 
     )
 
+
+
+
+
+
+
+
+    # RETURN
     return LaunchDescription([
-        world_file_arg,
+        DeclareLaunchArgument(
+            'world',
+            default_value=PathJoinSubstitution([
+                velmobil_simulation, 
+                'worlds', 
+                'basic.sdf'
+            ]),
+            description='Full path to the SDF world file to load'
+        ),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='If true, use simulated clock'
+        ),
+        DeclareLaunchArgument(
+            'rviz',
+            default_value='true',
+            description='If true, rviz will launch'
+        ),
         
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
+                [PathJoinSubstitution([ros_gz_sim,
                                        'launch',
                                        'gz_sim.launch.py'])]),
             launch_arguments=[('gz_args', [world, ' -r -v 1'])]),
@@ -113,12 +155,9 @@ def generate_launch_description():
                 on_exit=[mecanum_drive_controller_spawner],
             )
         ),
+        
         bridge,
-        node_robot_state_publisher,
+        robot_state_publisher,
         gz_spawn_entity,
         rviz_node,
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='If true, use simulated clock'),
     ])
